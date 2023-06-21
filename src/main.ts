09d1f0e59dfcb470a647d84601581979c09cec4d
nomian2001 <thanhtung2833@gmail.com>
2023-06-21 12:16:02 +0700

    feat: update compression for api

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import compression from '@fastify/compress';
 
 import { AppModule } from './app.module';
 
@@ -14,6 +15,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  await app.register(compression,{ encodings: ['gzip', 'deflate'] });
 
   await app.listen(6000, '0.0.0.0');
 }
