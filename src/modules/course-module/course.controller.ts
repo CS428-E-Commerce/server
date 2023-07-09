be57db5725852bc195e532024295ba15507d4684
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-07-09 23:11:14 +0700

    delete bugs with course

--- src/modules/course-module/course.controller.ts
+++ src/modules/course-module/course.controller.ts
@@ -4,21 +4,21 @@ import { Int32, MixedList } from "typeorm";
 
 @Controller('courses')
 export class CourseController{
-    @Post(':all')
+    @Post()
     getCourse(@Body() available:boolean=true):MixedList<CourseEntity>{
         //TODO: GET RANDOM COURSE (MAX NUMBER OF COURSE DEFINED)
         return []
     }
 
-    @Post(':level')
-    getCourseWithLevel(@Body() level:Int32, available:boolean=true):MixedList<CourseEntity>{
-        //TODO: GET COURSE WITH LEVEL (MAX NUMBER OF COURSE DEFINED)
-        return []
-    }
+    // @Post()
+    // getCourseWithLevel(@Body() level:Int32, available:boolean=true):MixedList<CourseEntity>{
+    //     //TODO: GET COURSE WITH LEVEL (MAX NUMBER OF COURSE DEFINED)
+    //     return []
+    // }
 
-    @Post(':coach')
-    getCourseWithCoachID(@Body() coachID:string, available:boolean=true):MixedList<CourseEntity>{
-        //TODO: GET COURSE WITH ID OF COACH (MAX NUMBER OF COURSE DEFINED)
-        return []
-    }
+    // @Post()
+    // getCourseWithCoachID(@Body() coachID:string, available:boolean=true):MixedList<CourseEntity>{
+    //     //TODO: GET COURSE WITH ID OF COACH (MAX NUMBER OF COURSE DEFINED)
+    //     return []
+    // }
 }
