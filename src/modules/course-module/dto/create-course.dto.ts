f38f87433f7f1605d49646518eb89ba4a248510f
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-07-22 09:44:37 +0700

    error

--- src/modules/course-module/dto/create-course.dto.ts
+++ src/modules/course-module/dto/create-course.dto.ts
@@ -1,10 +1,52 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsDate, IsNumber, IsOptional, IsString } from "class-validator";
 
-export class CreateCourseDTO{ 
+export class UpdateCourseDTO{ 
     @ApiProperty()
     id: number;
 
+    @ApiProperty()
+    @IsString()
+    code: string;
+    
+    @ApiProperty()
+    coachId: number;
+    
+    @ApiProperty()
+    @IsString()
+    title: string;
+    
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    banner: string;
+    
+    @ApiProperty()
+    @IsString()
+    status: string;
+    
+    @ApiProperty()
+    @IsString()
+    level: string;
+    
+    @ApiProperty()
+    maxSlot: number;
+    
+    @ApiProperty()
+    cost: number;
+    
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    description: string;
+    
+    @ApiProperty()
+    @IsString()
+    @IsOptional()
+    zoomLink: string;
+}
+
+export class CreateCourseDTO{
     @ApiProperty()
     @IsString()
     code: string;
