6971be1fe70cd9712bc9c6416005d69d2df649b1
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-07-22 15:47:29 +0700

    update

--- src/modules/course-module/course.controller.ts
+++ src/modules/course-module/course.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
 import { CourseService } from "./course.service";
 import { CreateCourseDTO, CreateSchedulerDTO, UpdateCourseDTO } from "./dto";
 import { FindCourseDTO, GetID, FindScheduler } from "./dto/find-course.dto";
@@ -8,37 +8,37 @@ export class CourseController{
     constructor(private courseService: CourseService){}
 
     @Post('createCourse')
-    async createCourse(@Body() courseDto: CreateCourseDTO){
+    createCourse(@Body() courseDto: CreateCourseDTO){
         return this.courseService.createCourse(courseDto)
     }
 
     @Post('updateCourse')
-    async updateCourse(@Body() courseDto: UpdateCourseDTO){
+    updateCourse(@Body() courseDto: UpdateCourseDTO){
         return this.courseService.updateCourse(courseDto);
     }
 
     @Post('findCourse')
-    async findCourse(@Body() findCourseDTO: FindCourseDTO){
-        return this.courseService.findCourse(findCourseDTO)
+    findCourse(@Query() FilterCourse: FindCourseDTO){
+        return this.courseService.findCourse(FilterCourse)
     }
 
     @Post('deleteCourse')
-    async deleteCourse(@Body() getCourseID: GetID){
+    deleteCourse(@Body() getCourseID: GetID){
         return this.courseService.deleteCourse(getCourseID)
     }
 
     @Post('createScheduler')
-    async createScheduler(@Body() createScheduler: CreateSchedulerDTO){
+    createScheduler(@Body() createScheduler: CreateSchedulerDTO){
         return this.courseService.createScheduler(createScheduler);
     }
 
     @Post('findScheduler')
-    async findScheduler(@Body() scheduler: FindScheduler){
+    findScheduler(@Body() scheduler: FindScheduler){
         return this.courseService.findScheduler(scheduler)
     }
 
     @Post('deleteScheduler')
-    async deleteScheduler(@Body() schedulerID: GetID){
+    deleteScheduler(@Body() schedulerID: GetID){
         return this.courseService.deleteScheduler(schedulerID)
     }
 }
