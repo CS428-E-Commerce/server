a1c1aaeb5520edb2055627a8303da29550a827cf
nomian2001 <thanhtung2833@gmail.com>
2023-08-02 22:52:57 +0700

    feat: update migration for attendeNumber in course

--- src/constants/environment.constants.ts
+++ src/constants/environment.constants.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 // Database
-export const POSTGRES_HOST = process.env.POSTGRES_HOST || 'localhost';
+export const POSTGRES_HOST = process.env.POSTGRES_HOST || 'db';
 export const POSTGRES_PORT = +process.env.POSTGRES_PORT || 5432;
 export const POSTGRES_USERNAME = process.env.POSTGRES_USER || 'postgres';
 export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'postgres';
